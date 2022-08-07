2203eb79b5be2637616fbbafebcf30911f20ac22
Jaydip Patel <jaydip.lamprostech@gmail.com>
2022-08-07 13:26:34 +0530

    Home page changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./styles/fonts/Mars/Mars Bold.ttf";
 
 const root = document.getElementById("root");
 render(<App />, root);
@@ -9,4 +10,3 @@ render(<App />, root);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
